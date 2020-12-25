d2f5d09aa9d60dfa5ddb912ab333a946d075a136
unknown <mallik.kokkandla@gmail.com>
2020-12-25 11:26:36 +0530

    variable discussion

---
+++ demo.js
@@ -0,0 +1,24 @@
+
+
+     
+
+
+
+
+// for(var i=0;i<5; i++){
+      
+// }
+// console.log(i);
+
+// var data=100;
+// var data=200;
+// console.log(data);
+
+// const obj={sub:"ang"};
+// console.log(obj);
+
+// obj.sub = "ractJS";
+// console.log(obj);
+
+
+
