e1f9101d15f6d2d4b468a3092e53533618e9677f
mallikarjuna <mallik.kokkandla@gmail.com>
2021-01-09 11:12:25 +0530

    optaional parameters

--- demo.js
+++ demo.js
@@ -1,24 +1,49 @@
+// // setTimeout(function(){
+// //     console.log("i am boy ra");
+// // },3000)
+// // setTimeout(function(){
+// //     console,log("hello");
+// // }, 3000)
+
+// // setInterval(function() {
+// //     console.log("brother");
+// // }, 2000);
 
 
-     
+// // for(let i=0; i<5; i++){
+// //     setTimeout(function()  {console.log(i);
+        
+// //     }, i*5000);
+
+// // for(let i=0;i<7;i++){
+
+// // };
+// // console.log(i);
+
+// // var data=100;
+// // var data=200;
+// // var data=300;
+// // console.log(data);
 
 
 
 
-// for(var i=0;i<5; i++){
-      
+
+// // const data=100;
+// // data=200;
+// // console.log(data);
+
+
+
+// let f_one=()=>{
+//     return "welcome";
+
+// };
+// console.log(f_one());
+// console.log(f_one);
+// let  malli=()=>{
+
 // }
-// console.log(i);
-
-// var data=100;
-// var data=200;
-// console.log(data);
-
-// const obj={sub:"ang"};
-// console.log(obj);
-
-// obj.sub = "ractJS";
-// console.log(obj);
-
-
-
+// ()=>{
+    
+// }
